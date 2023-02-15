c1749cae627c987d9fc0bb8477af5fb44a181347
jewelle-tatad <jewelle.tatad@gmail.com>
2023-02-15 12:12:42 -0800

    dis might be broken ehehe

--- TestListExamples.java
+++ TestListExamples.java
@@ -11,11 +11,13 @@ class IsMoon implements StringChecker {
 
 public class TestListExamples {
   @Test(timeout = 500)
+
   public void testMergeRightEnd() {
     List<String> left = Arrays.asList("a", "b", "c");
     List<String> right = Arrays.asList("a", "d");
     List<String> merged = ListExamples.merge(left, right);
     List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
+    
     assertEquals(expected, merged);
   }
 }
--- grade.sh
+++ grade.sh
@@ -3,3 +3,31 @@ CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 rm -rf student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
+
+cd student-submission
+
+if [[ -e ListExamples.java ]]
+then
+    echo 'ListExamples.java found!!'
+else
+    echo 'ListExamples.java not found. Please check that ListExamples.java is in your submission.'
+    exit
+fi
+
+cd ..
+
+cp student-submission/ListExamples.java ./
+
+pwd
+
+javac -cp $CPATH *.java
+
+if [[ $? -neq 0 ]]
+then
+    echo 'Compilation error!!'
+
+    exit
+fi
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > grade.txt
+
