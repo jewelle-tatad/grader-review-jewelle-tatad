69fab6e9c48e85b416e07ce4f1bab9099aac9db5
jewelle-tatad <jewelle.tatad@gmail.com>
2023-03-01 11:42:05 -0800

    sdfhk

--- grade.sh
+++ grade.sh
@@ -1,4 +1,4 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH='.;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar'
 
 rm -rf student-submission
 git clone $1 student-submission
@@ -32,4 +32,3 @@ fi
 java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > grade.txt
 
 cat grade.txt
-
